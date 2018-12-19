36e6093ad760a7d5af8c8fac5b321ca71a6297d0
jC AUDE <jean-christophe.aude@cea.fr>
2018-12-19 16:52:39 +0100

    [NEW] add parallel calculations using parallelMap package [UPD] replace 1:nscale by a scales vector for MDE/RCMDE [UPD] set m=1 by default

--- R/MDE.R
+++ R/MDE.R
@@ -1,17 +1,18 @@
 ## #'    \item Azami H, Rostaghi M, Abasolo D, Escudero J (2017) "Refined Composite Multiscale Dispersion Entropy and its Application to Biomedical Signals". IEEE transactions on bio-medical engineering 64:2872–2879.
 
+require(parallelMap)
 
 #' Refined Composite Multiscale Dispersion Entropy
 #'
 #' This function calculates the refined composite multiscale dispersion entropy (RCMDE) of a univariate signal x
 #'
 #' @param x a univariate signal (vector)
-#' @param m the embedding dimension
+#' @param m the embedding dimension (default: 1)
 #' @param nc the number of classes (it is usually equal to a number between 3 and 9, we use 6 by default)
 #' @param tau the time lag (it is usually equal to 1)
-#' @param nscale the number of scale factors
+#' @param scales the scale factors as a vector (default 1:10)
 #'
-#' @return a vector of size 1 * Scale - the RCMDE of x
+#' @return a vector of size 1 * length(scales) - the RCMDE of x
 #' @export
 #'
 #' @references
@@ -23,23 +24,21 @@
 #' @examples
 #' data(EG_181117)
 #' RCMDE(EG_181117)
-RCMDE <- function(x,m,nc=6,tau=1,nscale=5) {
+RCMDE <- function(x,m=1,nc=6,tau=1,scales=1:10) {
 
   # init.
-  mde <- vector(mode = "numeric",length = nscale)
-  mde[1:nscale] <- NaN
   nx <- length(x)
   mu <- mean(x)
   sigma <- sd(x)
 
-  # scale 1
-  disen <- DispersionEntropy(x = x, ma = "NCDF", m = m, nc = nc, tau = tau)
-  mde[1] <- disen$disp.en
-
   # other scales
-  if(nscale > 2) {
-    for(s in 2:nscale) {
-      pdf <- lapply(1:s, function(ss) {
+  rcmde <- parallelSapply(scales, function(s) {
+    if(s == 1) {
+      disen <- DispersionEntropy(x = x, ma = "NCDF", m = m, nc = nc, tau = tau)
+      mde <- disen$disp.en
+    }
+    else {
+      pdf <- parallelLapply(1:s, function(ss) {
         xs <- x[ss:nx]
         xs <- ScaleX(x = xs, scale = s)
         disen <- DispersionEntropy(x = xs, ma = "NCDF", m = m, nc = nc,
@@ -50,12 +49,14 @@ RCMDE <- function(x,m,nc=6,tau=1,nscale=5) {
       pdf <- matrix(pdf,nrow = s,ncol = length(pdf)/s, byrow = TRUE)
       pdf <- apply(pdf,2,mean)
       pdf <- pdf[pdf != 0]
-      mde[s] <- -sum(pdf * log(pdf))
+      mde <- -sum(pdf * log(pdf))
     }
-  }
+    return(mde)
+  })
+  names(rcmde) <- scales
 
   # eop
-  return(mde)
+  return(rcmde)
 }
 
 
@@ -64,12 +65,12 @@ RCMDE <- function(x,m,nc=6,tau=1,nscale=5) {
 #' This function calculates the multiscale dispersion entropy (MDE) of a univariate signal x
 #'
 #' @param x a univariate signal (vector)
-#' @param m the embedding dimension
+#' @param m the embedding dimension (default: 1)
 #' @param nc the number of classes (it is usually equal to a number between 3 and 9, we use 6 by default)
 #' @param tau the time lag (it is usually equal to 1)
-#' @param nscale the number of scale factors
+#' @param scales the scale factors as a vector (default 1:10)
 #'
-#' @return a vector of size 1 * Scale - the MDE of x
+#' @return a vector of size 1 * length(scales) - the MDE of x
 #' @export
 #'
 #' @references
@@ -81,26 +82,20 @@ RCMDE <- function(x,m,nc=6,tau=1,nscale=5) {
 #' @examples
 #' data(EG_181171)
 #' MDE(EG_181117)
-MDE <- function(x,m,nc=6,tau=1,nscale=5) {
+MDE <- function(x,m=1,nc=6,tau=1,scales=1:10) {
 
   # init.
-  mde <- vector(mode = "numeric",length = nscale)
   mu <- mean(x)
   sigma <- sd(x)
 
-  # scale 1
-  disen <- DispersionEntropy(x = x, ma = "NCDF", m = m, nc = nc, tau = tau)
-  mde[1] <- disen$disp.en
-
-  # scale 2...
-  if(nscale > 2) {
-    for(s in 2:nscale) {
-      xs <- ScaleX(x = x, scale = s)
-      disen <- DispersionEntropy(x = xs, ma = "NCDF", m = m, nc = nc, tau = tau,
-                                 mu = mu, sigma = sigma)
-      mde[s] <- disen$disp.en
-    }
-  }
+  # calc.
+  mde <- parallelSapply(scales, function(s) {
+    xs <- ScaleX(x = x, scale = s)
+    disen <- DispersionEntropy(x = xs, ma = "NCDF", m = m, nc = nc, tau = tau,
+                               mu = mu, sigma = sigma)
+    return(disen$disp.en)
+  })
+  names(mde) <- scales
 
   # eop
   return(mde)
@@ -108,12 +103,20 @@ MDE <- function(x,m,nc=6,tau=1,nscale=5) {
 
 
 ScaleX <- function(x,scale) {
+
+  # no-scaling
+  if(scale == 1)
+    return(x)
+
+  # scale signal
   xc <- seq_along(x)
   Rtrim <- max(which((xc %% scale) == 0))
   xc <- xc[1:Rtrim]
   x <- x[xc]
   xs <- split(x,ceiling(seq_along(x)/scale))
   xs <- sapply(xs,mean)
+
+  # eop
   names(xs) <- NULL
   return(xs)
 }
